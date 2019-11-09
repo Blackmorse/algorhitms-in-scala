b6956e5d309d2d0ad538063d6cacc5174c8e825f
Andrew <blackmorse@live.com>
2019-11-10 00:27:48 +0300

    1.4.38 Naive 3-sum

--- src/main/scala/one/four/NaiveThreeSum.scala
+++ src/main/scala/one/four/NaiveThreeSum.scala
@@ -1,15 +1,14 @@
 package one.four
 
-object ThreeSum {
+object NaiveThreeSum {
   def count(a: Array[Int]): Int = {
     val n = a.length
     var count = 0
-    for (i <- 0 until n; j <- i+1 until n; k <- j+1 until n) {
+    for (i <- 0 until n; j <- 0 until n; k <- 0 until n if i < j && j < k) {
       if (a(i).toLong + a(j) + a(k) == 0) count += 1
     }
     count
   }
-
 
   def main(args: Array[String]): Unit = {
     DoublingTest.doTest(100, 7, count)
