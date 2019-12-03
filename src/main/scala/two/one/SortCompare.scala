c64ac2bf85431df09e275c62b53eea24b0852c9c
Andrei Kochemirovskii <a.kochemirovskiy@corp.vk.com>
2019-12-03 20:44:29 +0300

    2.1.30 Geometric Increments

--- src/main/scala/two/one/SortCompare.scala
+++ src/main/scala/two/one/SortCompare.scala
@@ -19,9 +19,6 @@ object SortCompare {
   }
 
   def main(args: Array[String]): Unit = {
-
-    val a = Array(1,2,3)
-
     val n = 100000
     val t = 50
 
@@ -30,18 +27,28 @@ object SortCompare {
     while(h > 0)  {
       buf += h
       h = 3 * h + 1
-
     }
 
     val seq1 = buf.toSeq
     val seq2 = Seq(1, 5, 19, 41, 109, 209, 505, 929, 2161, 3905, 8929, 16001, 36289, 64769, 146305, 260609)
 
-//    timeRandomInput(new InsertionSort[Double](drawer = (a: Array[Double]) => ()), n, t)
-
     val t1 = timeRandomInput(new ShellSortArraySequence[Double](drawer = (a: Array[Double]) => (), seq1), n, t)
     val t2 = timeRandomInput(new ShellSortArraySequence[Double](drawer = (a: Array[Double]) => (), seq2), n, t)
 
     println(s"First/second: ${t1.toDouble / t2}")
+  }
 
+  def findDegreesSequence(): Unit = {
+    def degrees(d: Int, N: Int) = LazyList.unfold(1)(s => Some((s * d, s * d))).takeWhile(_ < N)
+
+    val n = 100000
+    val t = 5
+    //    timeRandomInput(new InsertionSort[Double](drawer = (a: Array[Double]) => ()), n, t)
+
+    for (d <- (2 to 100).reverse) {
+
+      val t1 = timeRandomInput(new ShellSortArraySequence[Double](drawer = (a: Array[Double]) => (), degrees(d, n)), n, t)
+      println(s"d = $d. Time: $t1")
+    }
   }
 }
